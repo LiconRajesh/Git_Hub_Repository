1a20693038c33be914f160b48394f23e41dd8d56
LiconRajesh <rkjena388@gmail.com>
2023-03-20 16:38:07 +0530

    fgfgfgfgff

---
+++ Git_Hub_Repository/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Git_Hub_Repository.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Content("Hii Rajesh");
+        }
+    }
+}
